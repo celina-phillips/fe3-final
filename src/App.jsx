5eefde959f9d1f08dcc6905b8da93524cec397c1
celina-phillips <phillipscelina98@gmail.com>
2023-04-04 14:22:03 -0300

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home.jsx";
+import Detail from "./Routes/Detail.jsx"
+import Favs from "./Routes/Favs.jsx"
+import Contact from "./Routes/Contact.jsx"
+
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/detail" element={<Detail/>}/>
+            <Route path="/favs" element={<Favs/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
           <Footer/>
       </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -7,6 +9,10 @@ const Navbar = () => {
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to='/'><h3>Home</h3></Link>
+      <Link to='/contact'><h3>Contact</h3></Link>
+      <Link to='/detail'><h3>Detail</h3></Link>
+      <Link to='/favs'><h3>Favs</h3></Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
