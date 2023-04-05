ba8b9eb29506452e79325d1b3d439638f6e5d661
celina-phillips <phillipscelina98@gmail.com>
2023-04-05 16:17:34 -0300

    termiando

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -3,7 +3,7 @@ import Card from '../Components/Card'
 import { useGlobalStates } from '../Components/utils/Context.jsx'
 
 
-
+//
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
